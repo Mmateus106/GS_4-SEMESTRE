b80bfa0b2a485c78832a83e769b91394976c3287
Mateus Mantovani Araújo <126684801+Mmateus106@users.noreply.github.com>
2024-11-12 11:08:01 -0300

    Projeto da GS criado no Spring initializer

---
+++ GlobalSolutionJava/src/main/java/br/com/fiap/GlobalSolutionJava/GlobalSolutionJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.GlobalSolutionJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalSolutionJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalSolutionJavaApplication.class, args);
+	}
+
+}
